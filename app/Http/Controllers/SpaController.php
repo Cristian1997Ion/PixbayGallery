28012099365d532ce37eea079aa0eeb7322f0e01
cristianion <cristian.ion@vivre.eu>
2021-03-10 16:27:42 +0200

    refactor: remove unused import

--- app/Http/Controllers/SpaController.php
+++ app/Http/Controllers/SpaController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
+/**
+ * Class SpaController
+ * @package App\Http\Controllers
+ */
 class SpaController extends Controller
 {
     public function index(): string
