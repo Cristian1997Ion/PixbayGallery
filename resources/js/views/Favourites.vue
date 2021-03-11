bc3f544c1c0783f1e29945b9decd3d004ee7fe0f
cristianion <cristian.ion@vivre.eu>
2021-03-11 15:47:16 +0200

    feat: adds basic favourites page

---
+++ resources/js/views/Favourites.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div v-if="!authenticated">In order to view this page, please authenticate.</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Favourites",
+
+    computed: {
+        auth: function () {
+            return this.$store.state.auth;
+        },
+
+        authenticated: function () {
+            return this.auth.user.token !== null;
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
