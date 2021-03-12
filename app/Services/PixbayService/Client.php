323638ee36f81c7e20e1653ce66150d7eb50723d
cristianion <cristian.ion@vivre.eu>
2021-03-12 16:26:56 +0200

    refactor

--- app/Services/PixbayService/Client.php
+++ app/Services/PixbayService/Client.php
@@ -33,9 +33,18 @@ class Client
         $this->guzzle = new \GuzzleHttp\Client(['base_uri' => $config->getDomain() . '.kikiki']);
     }
 
+    /**
+     * @param $page
+     * @param string $searchTerm
+     * @return GenericPhotosResponse
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws Exception
+     */
     public function getPhotos($page, $searchTerm = ''): GenericPhotosResponse
     {
-        $cacheKey = "pixbay_photos_{$searchTerm}_{$page}";
+        $cacheKey  = "pixbay_photos_{$searchTerm}_{$page}";
+        $cacheTime = 3600 * 24;
+
         if (!$response = Cache::get($cacheKey)) {
             $response = $this->guzzle->get(
                 $this->config->getEndpoint('photos'),
@@ -49,18 +58,8 @@ class Client
                 ]
             );
 
-            if ($response->getStatusCode() !== 200) {
-                throw new Exception("Something went wrong while downloading the photos..");
-            }
-
-            $response = [
-                'contents'    => $response->getBody()->getContents(),
-                'cached_at'   => Carbon::now('3')->toString(),
-                'cached_time' => 24 * 3600,
-                'cache_key'   => $cacheKey
-            ];
-
-            Cache::put($cacheKey, $response, 24 * 3600);
+            $response = self::handleResponse($response, $cacheKey, $cacheTime);
+            Cache::put($cacheKey, $response, $cacheTime);
         }
 
         return new GenericPhotosResponse($response);
@@ -74,7 +73,8 @@ class Client
      */
     public function getPhoto($id): Photo
     {
-        $cacheKey = "photo_{$id}";
+        $cacheKey  = "photo_{$id}";
+        $cacheTime = 24 * 3600;
         if (!$response = Cache::get($cacheKey)) {
             $response = $this->guzzle->get(
                 $this->config->getEndpoint('photos'),
@@ -86,18 +86,8 @@ class Client
                 ]
             );
 
-            if ($response->getStatusCode() !== 200) {
-                throw new Exception("Something went wrong while downloading the photos...");
-            }
-
-            $response = [
-                'contents'    => $response->getBody()->getContents(),
-                'cached_at'   => Carbon::now('3')->toString(),
-                'cached_time' => 24 * 3600,
-                'cache_key'   => $cacheKey
-            ];
-
-            Cache::put($cacheKey, $response, 24 * 3600);
+            $response = self::handleResponse($response, $cacheKey, $cacheTime);
+            Cache::put($cacheKey, $response, $cacheTime);
         }
 
         $photos = App::make(GenericPhotosResponse::class, ['response' => $response])->getPhotos();
@@ -106,5 +96,27 @@ class Client
         }
 
         return new Photo($photos[0]['id'], $photos[0]['user'], $photos[0]['url']);
+    }
+
+    /**
+     * @param $response
+     * @param $cacheKey
+     * @param $cacheTime
+     * @throws Exception
+     */
+    protected static function handleResponse($response, $cacheKey, $cacheTime): array
+    {
+        if ($response->getStatusCode() !== 200) {
+            throw new Exception("Something went wrong while downloading the photos...");
+        }
+
+        $response = [
+            'contents'    => $response->getBody()->getContents(),
+            'cached_at'   => Carbon::now('3')->toString(),
+            'cached_time' => $cacheTime,
+            'cache_key'   => $cacheKey
+        ];
+
+        return $response;
     }
 }
